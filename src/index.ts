52d6764f07e132d543895a5e900214fa90c46f26
Film_ <pawich23013@gmail.com_>
2025-09-26 17:02:11 +0700

    done2

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,32 @@ const app: any = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.listen(3000, () =>
-  console.log("🚀 Server running on http://localhost:3000")
-);
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: "lab 15 API service successfully"
+  });
+});
 
-app.get("/me", (req: Request, res: Response) =>{
+app.get("/me", (req: Request, res: Response) => {
   res.json({
     success: true,
     message: "Student Information",
     data: {
-    studentId: "670610720",
-    firstName: "Phavit",
-    lastName: "Wongdao",
-    prodram: "CPE",
-    section: "001"
-  }
+      studentId: "670610720",
+      firstName: "Phavit",
+      lastName: "Wongdao",
+      prodram: "CPE",
+      section: "001"
+    }
   })
 });
 
 app.use("/api/v2", studentRoutes);
 app.use("/api/v2", courseRoutes)
+
+app.listen(3000, () =>
+  console.log("🚀 Server running on http://localhost:3000")
+);
 
 export default app;
